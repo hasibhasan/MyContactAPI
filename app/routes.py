f0bf878fdd993015b8a97644d1a29df5a4663fb4
hhasandt <hhasan@dohatec.net>
2017-03-02 13:50:05 +0600

    v11

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,36 @@
-from flask import jsonify
+from flask import jsonify, request
 from app.models import User
 from flask_restful import Resource
 
 class UsersList(Resource):
        def get(self):
-        users_query = User.query.all()
-        print users_query
-        return jsonify()
+        results = User.query.all()
+
+        json_results = []
+        for result in results:
+            d = {'title': result.title,
+                 'description': result.body
+                }
+            json_results.append(d)
+
+        return jsonify(items=json_results)
+
+       def post(self):
+           user_data_json= request.get_json(force= True)
+           title= user_data_json['title']
+           description = user_data_json['description']
+           try:
+             user = User("Title 3", "Description")
+             user.add(user)
+             return {"result": "Successful"}
+           except:
+               return {"result":"Error"}
+
+
+
+
+
+
 
 
 
